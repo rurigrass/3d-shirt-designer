6f26b37a64549eb0177502158715f23b1139b393
rurigrass <ruripg@gmail.com>
2023-04-28 16:45:14 +0200

    3d shirt completed

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -2,7 +2,6 @@ import { useRef } from "react";
 import { easing } from "maath";
 import { useSnapshot } from "valtio";
 import { useFrame } from "@react-three/fiber";
-import { Decal, useGLTF, useTexture } from "@react-three/drei";
 
 import state from "../store";
 
